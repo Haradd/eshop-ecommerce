eb72fa3d8f6c3a99f607e466784876a2098cbc8e
Błażej Piaskowski <haradd@users.noreply.github.com>
2020-06-13 23:08:40 +0200

    move dotenv config to dev only

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module Eshop
     config.load_defaults 5.2
 
     config.active_record.schema_format = :sql
-    Dotenv::Railtie.load
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
