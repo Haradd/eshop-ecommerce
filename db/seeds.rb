55fdab76f52258eb591fed9880e3707a7501e1de
Błażej Piaskowski <haradd@users.noreply.github.com>
2020-06-14 23:28:41 +0200

    add seeds to create products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+
+12.times do
+  Product.create(
+    name: FFaker::Lorem.words(rand(3..6)).join(" "),
+    description: FFaker::Lorem.sentence,
+    price: rand(9..109).round(2)
+  )
+end
